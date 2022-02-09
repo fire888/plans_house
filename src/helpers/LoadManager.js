6b7439092bda23133ce44394c8d165adb7f0b8cc
vasilii <porokh.vs@gmail.com>
2022-02-10 02:25:34 +0300

    process

--- src/helpers/LoadManager.js
+++ src/helpers/LoadManager.js
@@ -6,43 +6,6 @@ import 'three/examples/js/loaders/FBXLoader'
 const Zlib = require("three/examples/js/libs/inflate.min"); // util for FBX loader
 window.Zlib = Zlib.Zlib;
 
-import '../assets/progress-img.png'
-import '../assets/icon-map.png'
-
-import sceneSrc from '../assets/interier/scene.FBX'
-import view1Src from '../assets/interier/001.jpg'
-import view2Src from '../assets/interier/002.jpg'
-import view3Src from '../assets/interier/003.jpg'
-import view4Src from '../assets/interier/004.jpg'
-
-const assetsToLoad = [
-    {
-        type: 'fbx',
-        filename: sceneSrc,
-        key: 'scene'
-    },
-    {
-        type: 'img',
-        filename: view1Src,
-        key: 'view1'
-    },
-    {
-        type: 'img',
-        filename: view2Src,
-        key: 'view2'
-    },
-    {
-        type: 'img',
-        filename: view3Src,
-        key: 'view3'
-    },
-    {
-        type: 'img',
-        filename: view4Src,
-        key: 'view4'
-    },
-]
-
 
 export function createLoadManager (eventEmitter) {
 
